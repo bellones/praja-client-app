dc50feb0562cb6a04ad0bd88814d5a267c0e4425
Thiago  Belao <thiagobelao@gmail.com>
2025-12-09 16:41:28 -0300

    feat: start scafolding home

--- src/components/forgotpassword/index.ts
+++ src/components/forgotpassword/index.ts
@@ -6,3 +6,4 @@ export { default as ForgotPasswordForm } from './ForgotPasswordForm';
 
 
 
+
--- src/features/tabs/screens/home/HomeScreen.tsx
+++ src/features/tabs/screens/home/HomeScreen.tsx
@@ -1,16 +1,21 @@
 
-import { Text } from 'react-native';
+import { useMemo } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Banner from '../../../../components/banner/Banner';
+import UserProfile from '../../../../components/user/UserProfile';
+import { useAuthStore } from '../../../../state';
 import { useTheme } from '../../../../theme/ThemeProvider';
 import createHomeStyles from './HomeStyles';
 
 const HomeScreen = () => {
   const { theme } = useTheme();
-  const styles = createHomeStyles(theme);
-  return (
+  const user = useAuthStore((state) => state.user);
+  const styles = useMemo(() => createHomeStyles(theme), [theme]);
+  return (  
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Início</Text>
-      </SafeAreaView>
+       <UserProfile user={user} /> 
+       <Banner />
+    </SafeAreaView>
   )
 }
 
